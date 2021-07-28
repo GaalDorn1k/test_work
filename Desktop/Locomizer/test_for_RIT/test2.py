ebaca44802e6e66325afecec03fd5726325f1d59
GaalDornik <beznosov.paul@yandex.ru>
2021-07-28 15:48:10 +0400

    v2

--- Desktop/Locomizer/test_for_RIT/test2.py
+++ Desktop/Locomizer/test_for_RIT/test2.py
@@ -26,7 +26,9 @@ def main():
     data_price = pd.read_csv('Приложение C', delimiter= '\t')
     data_time = pd.read_csv('Приложение B',delimiter= '\t')    
 
-
+    data_date.columns = ['Date', 'Performer', 'Task', 'Time']
+    data_price.columns = ['Performer', 'Rate']
+    data_time.columns = ['Task', 'Estimate']
 
 
 
@@ -34,7 +36,7 @@ def main():
 
     # Average hours per day by worker
     def average_hours_per_day(worker):         
-        amount_days = len(data_date[data_date['Исполнитель'] == worker].groupby(data_date['Дата']))
+        amount_days = len(data_date[data_date['Performer'] == worker].groupby(data_date['Date']))
         average_time = work_time[work_time.index == worker] / amount_days
         average_time[0] = "%.1f" % average_time[0]
         return average_time[0]
@@ -42,7 +44,7 @@ def main():
 
     # Average task time by worker
     def average_hours_for_task(worker):         
-        amount_tasks = len(data_date[data_date['Исполнитель'] == worker].groupby(data_date['Задача']))
+        amount_tasks = len(data_date[data_date['Performer'] == worker].groupby(data_date['Task']))
         average_time = work_time[work_time.index == worker] / amount_tasks
         average_time[0] = "%.1f" % average_time[0]
         return average_time[0]
@@ -62,8 +64,8 @@ def main():
 
     # mean deviation time for worker
     def mean_deviation_time(worker):            
-        worker = (data_date.loc[data_date['Исполнитель'] == worker])
-        worker_tasks = worker.loc[ :, ['Задача'][0]]
+        worker = (data_date.loc[data_date['Performer'] == worker])
+        worker_tasks = worker.loc[ :, ['Task'][0]]
         worker_tasks = pd.unique(worker_tasks)
         deviation = delta_time[worker_tasks].sum() / len(delta_time[worker_tasks])
         deviation = "%.2f" % deviation
@@ -74,20 +76,20 @@ def main():
     ################################################################################################################
 
     # work time for each worker
-    work_time = data_date['Часы'].groupby(data_date['Исполнитель']).sum()                                         
+    work_time = data_date['Time'].groupby(data_date['Performer']).sum()                                         
 
     # time for each task
-    tasks_time = data_date['Часы'].groupby(data_date['Задача']).sum()                                           
+    tasks_time = data_date['Time'].groupby(data_date['Task']).sum()                                           
     tasks_time = tasks_time.iloc[tasks_time.index.str.extract('(\d+)', expand=False).astype(int).argsort()]
     tasks_time.index = np.arange(0, len(tasks_time))
 
     # mean deviation time for each task
-    delta_time = data_time.loc[:,'Оценка'] - tasks_time
-    delta_time.index = data_time['Задача']
+    delta_time = data_time.loc[:,'Estimate'] - tasks_time
+    delta_time.index = data_time['Task']
 
 
     # Project time
-    project_time = int(data_date.loc[ :, ['Часы'] ].sum())
+    project_time = int(data_date.loc[ :, ['Time'] ].sum())
 
     # Average task time
     average_time_task = tasks_time.sum()/len(tasks_time)                         
@@ -97,11 +99,11 @@ def main():
 
 
     # workers dates
-    workers_dates = data_date['Дата'].groupby(data_date['Исполнитель']).apply(pd.DataFrame)
+    workers_dates = data_date['Date'].groupby(data_date['Performer']).apply(pd.DataFrame)
 
 
     # all dates
-    dates = data_date['Дата']
+    dates = data_date['Date']
     dates = pd.unique(dates)
     dates.sort()
 
@@ -114,8 +116,8 @@ def main():
     # costs = worker_time * price
 
     costs = 0
-    for i in range(len(data_price['Исполнитель'])):
-        costs += work_time[i] * data_price.loc[i]['Ставка']
+    for i in range(len(data_price['Performer'])):
+        costs += work_time[i] * data_price.loc[i]['Rate']
         profit = income - costs
         profitability = (profit * 100) / income
 
@@ -125,17 +127,17 @@ def main():
 
 
 
-    d = {'average_hours_per_day':   [average_hours_per_day(x)   for x in data_price['Исполнитель']],
-        'average_hours_for_task':  [average_hours_for_task(x)  for x in data_price['Исполнитель']],
-        'mean_deviation_time':     [mean_deviation_time(x)     for x in data_price['Исполнитель']],
-        'absence_days':            [absence_days(x)            for x in data_price['Исполнитель']]}
+    d = {'average_hours_per_day':   [average_hours_per_day(x)   for x in data_price['Performer']],
+        'average_hours_for_task':  [average_hours_for_task(x)  for x in data_price['Performer']],
+        'mean_deviation_time':     [mean_deviation_time(x)     for x in data_price['Performer']],
+        'absence_days':            [absence_days(x)            for x in data_price['Performer']]}
 
 
     data_output = pd.DataFrame(data=d)
     data_output.columns = ['Среднее время работы в день', 'Среднее время решения задачи', 'Среднее отклонение', 'Прогулы']
-    data_output.index = data_price['Исполнитель']
+    data_output.index = data_price['Performer']
 
-    print(data_output)
+    print(data_output)   
     print('\nТрудозатраты на проект в часах:', project_time)
     print('Среднее время, затраченное на решение задач в часах:', "%.1f" % average_time_task)
     print('Медианное время, затраченное на решение задач в часах:', median_time_task)
@@ -147,7 +149,7 @@ def main():
 
 
     x = np.arange(1, len(data_time)+1)
-    y = data_time['Оценка']
+    y = data_time['Estimate']
     yy = tasks_time
     plt.rcParams['figure.figsize'] = (15.0, 8.0)
     plt.title('Оценка трудозатрад')
